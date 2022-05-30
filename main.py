ec4e6cc648a939a60af545af4b6bca9933ccd702
Temitope Yusuff <62524611+Tope-stack@users.noreply.github.com>
2022-05-30 15:21:23 +0000

    second commit

--- main.py
+++ main.py
@@ -74,3 +74,32 @@ print(SoftwareEngineer.entry_salary(24))
 # class vs instance
 # instance attribute defined in __init__(self)
 #class attribute
+# inherits, extend, override
+class Employee:
+  
+  def __init__(self, name, age, salary):
+    self.name = name
+    self.age = age
+    self.salary = salary
+
+  def work(self):
+    print(f"{self.name} is working...")
+
+
+class SoftwareEngineer(Employee):
+
+  def __init__(self, name, age, salary, level):
+    super().__init__(name, age, salary)
+    self.level = level
+  
+
+class Designer(Employee):
+  pass
+  
+
+se = SoftwareEngineer("Max", 25, 6000, "Junior")
+se.work()
+print(se.level)
+
+d = Designer("Philip", 27, 7000)
+d.work()
