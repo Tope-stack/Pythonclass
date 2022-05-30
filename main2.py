9c0bf59d61682c25d7af0adc5bda771593653257
Temitope Yusuff <62524611+Tope-stack@users.noreply.github.com>
2022-05-30 14:48:50 +0000

    first commit

--- main2.py
+++ main2.py
@@ -1,11 +1,29 @@
 
 # inherits, extend, override
 class Employee:
-  pass
+  
+  def __init__(self, name, age, salary):
+    self.name = name
+    self.age = age
+    self.salary = salary
+
+  def work(self):
+    print(f"{self.name} is working...")
 
 
 class SoftwareEngineer(Employee):
-  pass
+
+  def __init__(self, name, age, salary, level):
+    super{}.__init__(name, age, salary)
+    self.level = level
+  
 
 class Designer(Employee):
   pass
+  
+
+se = SoftwareEngineer("Max", 25, 6000, "Junior")
+se.work()
+
+d = designer("Philip", 27, 7000)
+d.work()
